387fa6bbf1efb656f7e961bda6c25d061f77c10c
MB <michael@michaelbaycura.com>
2021-12-12 00:55:08 -0500

    Initial menu structure worked out, have some back and forth menu behavior working.

--- SlotCarRaceController.ino
+++ SlotCarRaceController.ino
@@ -37,10 +37,70 @@ byte pin_column[KP_COLS] = {9,10,11,12};
 Keypad keypad = Keypad( makeKeymap(keys), pin_rows, pin_column, KP_ROWS, KP_COLS );
 
 
+// **** Global Variables ****
+// create enum to hold possible state values
+enum states {
+  Menu,
+  Race,
+  Paused
+};
 
-void PrepareKeyPad(){
-  
+char MenuKeys[] = {'A', 'B', 'C', 'D'};
+
+
+//****** Menu String Arrays **********
+const char* MainMenuText[4] = {
+  "A Change Settings",
+  "B Select a Race",
+  "C See Results",
+  ""
+};
+const char* SettingsMenuText[4] = {
+  "A Set Race Time",
+  "B Set Lap Number",
+  "C Set Lanes",
+  "D   <-- Return"
+};
+const char* RaceSelectMenuText[4] = {
+  "A Start Timed Race",
+  "B Start Lap Race",
+  "C Start Pol Trials",
+  "D   <-- Return"
+};
+const char* ResultMenuText[4] = {
+  "A Set Race Time",
+  "B Set Lap Number",
+  "C Set Lanes",
+  "D   <-- Return"
+};
+
+// create variale to hold current 'state'
+states state = Menu;
+
+
+void UpdateMenu(char *curMenu[]){
+  // depending on the current menu map key press to next menu
+  // for each string in the menu array, print it to the screen
+  // clear screen in case new text doesn't cover all old text
+  lcd.clear();
+  for (int i=0; i<4; i++){
+    lcd.setCursor(1,i);
+    lcd.print(curMenu[i]);
+  }
 }
+
+enum Menus {
+  MainMenu,
+  SettingsMenu,
+  RaceSelectMenu,
+  ResultMenu,
+  SetRaceTime,
+  SetRaceLaps,
+  SetLanes
+};
+
+Menus currentMenu;
+
 // Initialize hardware and establish software initial state
 void setup(){
   int status;
@@ -49,7 +109,8 @@ void setup(){
 	// to determine if initalization failed.
 	// the actual status codes are defined in <hd44780.h>
 	status = lcd.begin(LCD_COLS, LCD_ROWS);
-	if(status) // non zero status means it was unsuccesful
+  // non zero status means it was unsuccesful init
+	if(status) 
 	{
 		// begin() failed so blink error code using the onboard LED if possible
 		hd44780::fatalError(status); // does not return
@@ -58,30 +119,99 @@ void setup(){
   lcd.clear();
   // open connection on serial port
   Serial.begin(9600);
+  currentMenu = MainMenu;
+  UpdateMenu(MainMenuText);
+  
+          Serial.println(currentMenu);
+          Serial.println(SettingsMenu);
 }
 
-int prevMills;
-int timer;
 
 void loop(){
-  loadMainMenu();
-  timer = millis();
-  lcd.setCursor(5,3);
-  lcd.print(timer);
 
-  char key = keypad.getKey();
-  if (key){
-    Serial.println(key);
+  switch (state) {
+    case Menu:
+      // monitor keypad for user inputs
+      char key = keypad.getKey();
+      // if a press is detected then updated the menu
+      if (key) {
+        switch (currentMenu) {
+          case MainMenu:
+            switch (key) {
+              case 'A':
+                currentMenu = SettingsMenu;
+                Serial.println("set" + SettingsMenu);
+                UpdateMenu(SettingsMenuText);
+                break;
+              case 'B':
+                currentMenu = RaceSelectMenu;
+                UpdateMenu(RaceSelectMenuText);
+                break;
+              case 'C':
+                currentMenu = ResultMenu;
+                UpdateMenu(ResultMenuText);
+                break;
+              default:
+                break;
+            }
+            break;
+          case SettingsMenu:
+            switch (key) {
+              case 'A':
+                currentMenu = SetRaceTime;
+                UpdateMenu(MainMenuText);
+                break;
+              case 'B':
+                currentMenu = SetRaceLaps;
+                UpdateMenu(MainMenuText);
+                break;
+              case 'C':
+                currentMenu = SetLanes;
+                UpdateMenu(MainMenuText);
+                break;
+              case 'D':
+                currentMenu = MainMenu;
+                UpdateMenu(MainMenuText);
+              default:
+                break;
+            }
+            break;
+          case RaceSelectMenu:
+            break;
+          case ResultMenu:
+            break;
+          default:
+            break;
+        }; // end of Menu switch
+
+        Serial.println(key);
+        Serial.println(currentMenu);
+
+      }; // end of if key pressed wrap
+      break; // case of state = Menu 
+
+
+    case Race:
+
+      break;
+
+    case Paused:
+
+      break;
+
+    default:
+      // if the state becomes unknown then default back to 'Menu'
+      state = Menu;
+      break;
   }
 
-  // delay(1000);
 }
 
-void loadMainMenu(){
-  lcd.setCursor(1,0);
-  lcd.print("A Change Settings");
-  lcd.setCursor(1,1);
-  lcd.print("B Select Race Type");
-  lcd.setCursor(1,2);
-  lcd.print("C See Results");
-}
+// void loadMainMenu(){
+//   lcd.setCursor(1,0);
+//   lcd.print(MainMenu[0]);
+//   lcd.setCursor(1,1);
+//   lcd.print(MainMenu[1]);
+//   lcd.setCursor(1,2);
+//   lcd.print(MainMenu[2]);
+// }
